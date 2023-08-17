1a8a9b437e75a8eeb42594168d8fe2f6a2a97981
Max <sibo_jian_29903549@163.com>
2023-08-17 09:19:35 +0800

    feat: Initial yolov8-real-time project

---
+++ yolov8-real-time/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  yolov8-real-time
+//
+//  Created by JONO-Jsb on 2023/8/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
