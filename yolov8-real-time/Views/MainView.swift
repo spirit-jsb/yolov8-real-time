a1e86d100976aa3da209b61b964914a37ac76da7
Max <sibo_jian_29903549@163.com>
2023-08-22 09:12:23 +0800

    refactor: Change app lifecycle from UIKit to SwiftUI
    * Add YOLOv8RealTimeApp
    * Add MainView
    * Remove AppDelegate
    * Remove SceneDelegate
    * Remove Main.stroyboard
    * Remove LaunchScreen.stroyboard
    * Clean up Info.plist

---
+++ yolov8-real-time/Views/MainView.swift
@@ -0,0 +1,23 @@
+//
+//  MainView.swift
+//  yolov8-real-time
+//
+//  Created by JONO-Jsb on 2023/8/22.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct MainView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
---
+++ yolov8-real-time/YOLOv8RealTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YOLOv8RealTimeApp.swift
+//  yolov8-real-time
+//
+//  Created by JONO-Jsb on 2023/8/22.
+//
+
+import SwiftUI
+
+@main
+struct YOLOv8RealTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
